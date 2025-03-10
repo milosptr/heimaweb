6216bee1bf3059f366b2b9053eca8122c55264a3
milos <milosptr@hotmail.com>
2025-03-10 18:30:22 +0000

    resend apikey

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { FeedbackTemplate } from '@/components/EmailTemplates/FeedbackTemplate'
 import { Resend } from 'resend'
 import { EmailBody } from '@/tyoes'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY || '')
 
 export async function POST(request: Request) {
   const body = (await request.json()) as EmailBody
